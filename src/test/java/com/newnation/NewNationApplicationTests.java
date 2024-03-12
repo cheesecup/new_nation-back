a8829af356c1738320323d4211f7bb044b339bb1
cheesecup <kdy8252@naver.com>
2024-03-12 16:56:36 +0900

    프로젝트 생성

---
+++ src/test/java/com/newnation/NewNationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newnation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewNationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
