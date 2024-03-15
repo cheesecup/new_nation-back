fcf91f9b53dbb82297c5ce5b0440de4171c0363b
Seungmi97 <mi.seung97@gmail.com>
2024-03-15 15:41:28 +0900

    [FEAT] : 유저 인증 확인 api 추가

--- src/main/java/com/newnation/member/dto/LoginResponseDTO.java
+++ src/main/java/com/newnation/member/dto/LoginResponseDTO.java
@@ -1,14 +1,17 @@
 package com.newnation.member.dto;
 
+import com.newnation.member.entity.MemberRoleEnum;
 import com.newnation.member.entity.ResponseMsg;
 import lombok.Getter;
 
 @Getter
 public class LoginResponseDTO extends ResponseDTO {
     private String token;
+    private MemberRoleEnum authType;
 
-    public LoginResponseDTO(String token, String msg) {
+    public LoginResponseDTO(String token, String msg, MemberRoleEnum authType) {
         super(msg);
         this.token = token;
+        this.authType = authType;
     }
 }
