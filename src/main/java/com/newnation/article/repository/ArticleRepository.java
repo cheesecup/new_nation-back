36faebc2df589d2f9561e72932cae934f05b1417
cheesecup <kdy8252@naver.com>
2024-03-16 22:34:06 +0900

    게시글 목록 조회 `ArticleId` 기준 내림차순 정렬

--- src/main/java/com/newnation/article/repository/ArticleRepository.java
+++ src/main/java/com/newnation/article/repository/ArticleRepository.java
@@ -7,5 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
-    List<Article> findByCategory(Category category);
+    List<Article> findByCategoryOrderByArticleIdDesc(Category category);
+
+    List<Article> findAllByOrderByArticleIdDesc();
 }
